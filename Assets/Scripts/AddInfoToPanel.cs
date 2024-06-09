8a58e6974d902cbdf0fd68835512b5f2bd4c880b
FelipeNoord <58918959+FelipeNoord@users.noreply.github.com>
2024-06-09 18:30:15 +0200

    final realisation commit

--- Assets/Scripts/AddInfoToPanel.cs
+++ Assets/Scripts/AddInfoToPanel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:391ead7e91034ab661f3ac85bcb5d3becd8af327c720e0eda8fbcd2b1b9ff87f
-size 11654
+oid sha256:b6e07ab1cc17cd590ab4bf21690c243280ac365fbfe6656f7e6bf0a6945494b6
+size 11648
--- Assets/Scripts/RaycastPos.cs
+++ Assets/Scripts/RaycastPos.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f16511ed70681020fc372be47e16cbaa333815fbe506ffe6f04c309a61a26b0e
-size 6782
+oid sha256:aae07c842142436bc841628ef3ac78815b483a0a968c07cdfebefeeb30eb95f5
+size 6781
