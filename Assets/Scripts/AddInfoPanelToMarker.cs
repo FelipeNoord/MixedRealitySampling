36a69080dd9dfca98d18b6eca8969f53a8af22d0
FelipeNoord <58918959+FelipeNoord@users.noreply.github.com>
2024-06-25 11:41:14 +0200

    final felipe commit

--- Assets/Scripts/AddInfoPanelToMarker.cs
+++ Assets/Scripts/AddInfoPanelToMarker.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee54dc9ef4263319c5e68068b81036c0808c6d87f58c82761fb1221106736052
-size 2274
+oid sha256:2e4eab91ed5d2b0c1198c32dc3e5e83cfe585c9a3815184d794c9bcdf123ae03
+size 2180
